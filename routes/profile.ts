cceb18aaf4dfc32a0d99b386686949dbf6ddb2e9
maryammzadeh <maryammzadeh@gmail.com>
2016-02-07 02:52:53 -0800

    Adding profile route and  updated index and app to be aware of the new route.

--- routes/profile.ts
+++ routes/profile.ts
@@ -1,5 +1,6 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
+//<reference path='../types/DefinitelyTyped/mongodb/mongodb-1.4.9.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/mongodb/mongodb.d.ts'/>
 
 
@@ -8,7 +9,7 @@ var router = express.Router();
 var ObjectID = require('mongodb').ObjectID;
 
 
-/* GET  Account settings. */
+/* GET  Profile settings. */
 router.get('/', function(req, res) {
     var db = req.db;
     var registeredUsers = db.get('registeredUsers');
@@ -16,36 +17,12 @@ router.get('/', function(req, res) {
 	 // Fetch the document
     registeredUsers.findOne({_id:req.headers['_id']}, function(err, user) {
 		if(user)
-		{			
-			res.render('account', { "accountsettings": user });
-			res.redirect("accountsettings");
-		}
-		else
-		{
-			res.send("ACCESS DENIED");
-		}
-	});
-	
-});
-
-/* POST Account setttings */
-router.post('/', function (req, res) {
-    var db = req.db;
-    var registeredUsers = db.get('registeredUsers');
-    // Fetch the document
-    registeredUsers.findOne({_id:req.headers['_id']}, function(err, user) {
-		if(user)
-		{
+		{		
 			if (user.accountType === "viewer") {
 				var viewers = db.get('viewers');
-				viewers.findOne({_id:req.headers['_id']}, function(err, viewer) {
+				viewers.findOne({_id:req.headers['_id']}, function(err, viewers) {
 					if (err) {
 						res.send("ACCESS DENIED");
-					}
-					else
-					{
-						// need to ask client to give format of data being sent up for account settings..
-						// are they only sending updated settings? or all.
 					}
 				});
 			}
@@ -55,21 +32,17 @@ router.post('/', function (req, res) {
 					if (err) {
 						res.send("ACCESS DENIED");
 					}
-					else
-					{
-						// need to ask client to give format of data being sent up for account settings..
-						// are they only sending updated settings? or all.
-					}
 				});
 			}	
-			res.render('accountsettings', { "accountsettings": user });
-			res.redirect("accountsettings");
+			res.render('profile', { "profile": user });
+			res.redirect("profile");
 		}
 		else
 		{
 			res.send("ACCESS DENIED");
 		}
-	}); 
+	});
+	
 });
 
 module.exports = router;
