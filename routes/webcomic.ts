4d2d69eb45397433874508b50e3a7339cdb41156
Angela Lu <azlialu@gmail.com>
2016-02-08 10:25:50 -0800

    modified post method in webcomic.ts to interact with the DB to insert web comic data into the DB

--- routes/webcomic.ts
+++ routes/webcomic.ts
@@ -6,11 +6,11 @@ import Comic = require('../models/Comic');
 var express = require('express');
 var router = express.Router();
 
-var db = req.db;
-var ComicCollection = db.get('ComicCollection');
-
 /* View Comic: Get from Comic DB */
 router.get('/:id', function(req, res) {
+    var db = req.db;
+    var ComicCollection = db.get('ComicCollection');
+
     // get web comic id from reqest parameter in the URL
     var comicID = req.params.id;
 
@@ -20,6 +20,7 @@ router.get('/:id', function(req, res) {
     // extract the value of each field from the comic document
     var title = doc.title;
     var author = doc.author;
+    var publicationDate = doc.publicationDate;
     var description = doc.description;
     var genre = doc.genre;
     var toPublish = doc.toPublish;
@@ -33,21 +34,31 @@ router.get('/:id', function(req, res) {
 
 /* Create Comic: Post Comic to ComicCollection and ComicCellCollection in DB */
 router.post('/create', function(req, res) {
+    var db = req.db;
+    var ComicCollection = db.get('ComicCollection');
     
-    // send dummy values back to client
-    var comicID = "111";
+    // extract values of new comic data fields
+    var title = req.body.title; 
+    var author = req.body.author;
+    var description = req.body.description; 
+    var genre = req.body.genre;
+    var toPublish = req.body.toPublish;
+    //var img = req.body.img;
 
-    res.send("created a web comic");
-/*
-    console.log("created a web comic");
+    // insert comic in DB
+    ComicCollection.insert({title: title, author: author, description: description, genre: genre, toPublish: toPublish}, function(err, doc) {
+        var comicID = doc._id;
+    });
 
-    // redirect client to created comic web page
-    res.redirect("111");
-    */
+    // redirect client to newly created web comic page
+    //res.redirect("111");
+    //res.render('viewer', { title: 'Welcome back!'});
 });
 
 /* Edit Comic: Patch Comic to ComicCollection and ComicCellCollection DB */
 router.put('/:id', function(req, res) {
+    var db = req.db;
+    var ComicCollection = db.get('ComicCollection');
     
     // get web comic id from reqest parameter in the URL
     var comicID = req.params.id;
@@ -60,6 +71,8 @@ router.put('/:id', function(req, res) {
 
 /* Delete Comic: Delete from ComicCollection and ComicCellCollection in DB */
 router.delete('/:id', function(req, res) {
+    var db = req.db;
+    var ComicCollection = db.get('ComicCollection');
 
  // get web comic id from reqest parameter in the URL
     var comicID = req.params.id;
