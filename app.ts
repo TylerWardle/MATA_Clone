c62e51e72ecd475e7b872b3420fa5195122be834
maryam <maryam>
2016-02-11 19:24:09 -0800

    Merging with master

--- app.ts
+++ app.ts
@@ -12,7 +12,6 @@ class Application {
     constructor() { }
 
     startApp() {
-
         var express = require('express');
         var path = require('path');
         var favicon = require('serve-favicon');
