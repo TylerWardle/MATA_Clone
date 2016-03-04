c7243bd8073e89698d477fa59764e329ae660d47
asiaschmok <asiaschmok@gmail.com>
2016-03-04 10:32:43 -0800

    merging with master

---
+++ routes/forgotpassword.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+/* GET forgotpassword page. */
+router.get('/', function (req, res) {
+    res.render('forgotpassword', { title: 'Recover your account' });
+});
+/* POST to  into the system. */
+router.post('/', function (req, res) {
+    var db = req.db;
+    var registeredUsers = db.get('registeredUsers');
+    registeredUsers.findOne({ username: req.body.username }, function (err, user) {
+        if (user) {
+            res.render('resetpassword', { title: 'Sign In!' });
+        }
+        else {
+            res.render("error", { message: "User does not exist" });
+        }
+    });
+});
+module.exports = router;
