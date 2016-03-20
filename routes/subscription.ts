e48930002a0b3c85f31ef7bd793fec16e6d994c5
maryam <maryam>
2016-03-20 12:37:32 -0700

    turing subscriptions route in to class

--- routes/subscription.ts
+++ routes/subscription.ts
@@ -2,23 +2,37 @@
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
 ///<reference path='../types/DefinitelyTyped/mongodb/mongodb.d.ts'/>
 
-var express = require('express');
-var router = express.Router();
-var ObjectID = require('mongodb').ObjectID;
 
 
-/* POST Subscribe to a contributor. */
-router.post('/', function (req, res) {
-    var db = req.db;
-    var registeredUsers = db.get('registeredUsers');
-});
+class Subscription {
+
+    constructor() { }
+
+    startSubscription() {
+		var express = require('express');
+		var router = express.Router();
+		var ObjectID = require('mongodb').ObjectID;
+
+
+		/* POST Subscribe to a contributor. */
+		router.post('/', function (req, res) {
+			var db = req.db;
+			var registeredUsers = db.get('registeredUsers');
+		});
 
 
 
-/* Delete unsubscribe to a contributor. */
-router.delete('/', function (req, res) {
-    var db = req.db;
-    var registeredUsers = db.get('registeredUsers');
+		/* Delete unsubscribe to a contributor. */
+		router.delete('/', function (req, res) {
+			var db = req.db;
+			var registeredUsers = db.get('registeredUsers');
 
-});
-module.exports = router;
+		});
+		module.exports = router;
+
+
+		}
+	}
+	
+var subscription = new Subscription();
+subscription.startSubscription();
