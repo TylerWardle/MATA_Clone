83b016410b8bcc5652a72e61fa6b5f07a0da73b4
maryam <maryam>
2016-03-24 16:06:00 -0700

    Adding endpoints for viewing history.

---
+++ routes/history.js
@@ -0,0 +1,27 @@
+///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
+///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
+/* This router is responsible for storing and retreving user history.
+    For now only  all history will be stored. */
+var ViewingHistory = (function () {
+    function ViewingHistory() {
+    }
+    ViewingHistory.prototype.startViewingHistory = function () {
+        var express = require('express');
+        var router = express.Router();
+        /* POST add a webcomic id to the users history list */
+        router.post('/', function (req, res) {
+            var db = req.db;
+            var registeredUsers = db.get('registeredUsers');
+            // when user got
+        });
+        /* GET the history of webcomics a user has  */
+        router.get('/', function (req, res) {
+            var db = req.db;
+            var registeredUsers = db.get('registeredUsers');
+        });
+        module.exports = router;
+    };
+    return ViewingHistory;
+})();
+var viewingHistory = new ViewingHistory();
+viewingHistory.startViewingHistory();
