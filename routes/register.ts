1d6989ebcbaaf4e07f570a2c45654c642dba7f37
maryam <maryam>
2016-03-30 22:59:19 -0700

    Updating register router and merging with tyler's changes

--- routes/register.ts
+++ routes/register.ts
@@ -19,15 +19,79 @@ class Register {
 
 		/* POST register (adds a new user to the system). */
 		router.post('/', function(req, res) {
+				
+		var db = req.db;
 
-			if(registeredUserServiceProvider.create(req, res))
-			{
-				res.redirect("signin");
-			}
-			else
-			{
-				res.render("error", { message: "username " + req.body.username+ " is already taken!" });
-			}
+		var registeredUsers = db.get('registeredUsers');
+	
+		registeredUsers.findOne({username:req.body.username}, function(err, item) {
+		if(item){
+			res.render("error", { message: "Username is taken, please choose another username. " });
+		} else{
+			
+			var username = req.body.username;
+			var firstName = req.body.firstName;
+			var lastName = req.body.lastName;
+			var accountType = req.body.accountType;
+			var password = req.body.password;
+			
+			registeredUsers.insert({
+				"username": req.body.username.toLowerCase(),
+				"firstName": req.body.firstName,
+				"lastName": req.body.lastName,
+				"accountType": req.body.accountType,
+				"password": req.body.password,
+				"securityQuestion": req.body.securityQuestion,
+				"securityAnswer": req.body.securityAnswer,
+				"profilePicture": "http://www.openshot.org/images/blank_profile.png",
+				"aboutMe": "Nothing has been added to this section yet..",
+				"lastLogin": (new Date()).toDateString(),
+				"webComicViewingHistory": "",
+				"subscriptions": "",
+				
+			}, function(err, doc) {
+				if (err) {
+					res.render("error", { message: "There was a problem adding the information to the database.1"});
+				} else 
+				{
+					if(accountType === "viewer")
+					{
+						var viewers = db.get('viewers');
+						//var newViewer = new Viewer(username, password, firstName, lastName, accountType);
+						
+						viewers.insert({
+						"username": doc.username,
+						"firstName": doc.firstName,
+						"lastName": doc.lastName,
+						"guid": doc._id
+							
+						}, function(err, doc) {
+							if (err) {
+								res.render("error", { message: "There was a problem adding the information to the database.2" });
+							}
+						})
+					}
+					else
+					{
+						var contributors = db.get('contributors');
+						//var newContributor = new Contributor(username, password, firstName, lastName, accountType);
+					
+						contributors.insert({						
+							"username": doc.username,
+							"firstName": doc.firstName,
+							"lastName": doc.lastName,
+							"guid": doc._id
+							
+						}, function(err, doc) {
+							if (err) {
+								res.render("error", { message: "There was a problem adding the information to the database.3"});
+							} 
+						})
+					}
+				}
+			})
+		}
+	});
 		});
 
 		/* GET register page. */
